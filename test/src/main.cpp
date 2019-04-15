6f15d1d16d62550564a6d64cb3998d4bc69c2b54
ClaudeTO80 <claudio.cavallero@gmail.com>
2019-04-15 13:11:02 +0200

    Improved Full Factorial test

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -13,7 +13,7 @@ using namespace AnalysisGenerator;
 
 int main()
 {
-	vector<int> levels;
+	/*vector<int> levels;
 	levels.push_back(2);
 	levels.push_back(4);
 	levels.push_back(3);
@@ -21,21 +21,19 @@ int main()
 	vector<pair<double,double>> bounds;
 	bounds.push_back(make_pair(0, 1));
 	bounds.push_back(make_pair(-1, 1));
-	bounds.push_back(make_pair(0, 10));
+	bounds.push_back(make_pair(0, 10));*/
 
 	AnalysisParametersBlock block;
 
 	block.addParameter("x", 0, 1);
 	block.addParameter("y", -1, 1);
 	block.addParameter("z", 0, 10);
-	/*AnalysisParameters params;
-
-	params.addParameter("x", 0, 1);
-	params.addParameter("y", -1, 1);
-	params.addParameter("z", 0, 10);*/
+	
 	auto temp = make_shared< AnalysisParametersBlock>(block);
 	FullFactorial ff(temp);
 	ff.generate();
+	auto mm=ff.getMatrix();
+	ff.dumpMatrix(R"(C:\tmp\matrix.dat)");
 	Model model;
 	//FullFactorialDoeGenerator sgdg(bounds);
 	//sgdg.setLevels(levels);
